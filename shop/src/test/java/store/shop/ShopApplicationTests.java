ccc5d35f31397a8c63d05a1283ac8c5a3bf44a5e
Gyuseok Kim <gyuseok6394@gmail.com>
2021-07-13 23:35:22 +0900

    2021-07-13 init project

---
+++ shop/src/test/java/store/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package store.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
