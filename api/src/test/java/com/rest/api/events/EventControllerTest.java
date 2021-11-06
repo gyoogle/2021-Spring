2f1fee3b11a3314c57484711cf6bca70553f1ebc
Gyuseok Kim <gyuseok6394@gmail.com>
2021-11-07 01:23:54 +0900

    [feature/restapi-hateoas] Add : hateoas 적용

--- api/src/test/java/com/rest/api/events/EventControllerTest.java
+++ api/src/test/java/com/rest/api/events/EventControllerTest.java
@@ -43,7 +43,9 @@ public class EventControllerTest {
                 .basePrice(100)
                 .maxPrice(200)
                 .limitOfEnrollment(100)
+                .eventStatus(EventStatus.DRAFT)
                 .location("D Start up Factory")
+
                 .build();
 
         mockMvc.perform(post("/api/events/")
@@ -54,7 +56,14 @@ public class EventControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("id").exists())
                 .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE));
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("free").value(false))
+                .andExpect(jsonPath("offline").value(true))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.query-events").exists())
+                .andExpect(jsonPath("_links.update-events").exists());
+
     }
 
     @Test
