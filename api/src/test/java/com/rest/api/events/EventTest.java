d0c578ff36de631e39fdeed026d859fa77596f6a
Gyuseok Kim <gyuseok6394@gmail.com>
2021-11-07 00:44:37 +0900

    [feature/junitparam] Add : JunitParam을 활용한 Test

--- api/src/test/java/com/rest/api/events/EventTest.java
+++ api/src/test/java/com/rest/api/events/EventTest.java
@@ -1,9 +1,13 @@
 package com.rest.api.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
     @Test
@@ -29,5 +33,54 @@ public class EventTest {
         assertThat(event.getName()).isEqualTo(name);
         assertThat(event.getDescription()).isEqualTo(description);
 
+    }
+
+    @Test
+    @Parameters
+    //@Parameters(method = "parametersForTestFree")
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
+        //given
+        Event event = Event.builder().
+                basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
+
+    // 자동 매칭
+    private Object[] parametersForTestFree() {
+        return new Object[] {
+                new Object[] {0, 0, true},
+                new Object[] {100, 0, false},
+                new Object[] {0, 100, false},
+        };
+    }
+
+    @Test
+    @Parameters
+    public void testOffline(String location, boolean isOffline) {
+        //given
+        Event event = Event.builder()
+                .location("StartUp Factory")
+                .build();
+
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isOffline()).isTrue();
+    }
+
+    private Object[] parametersForTestOffline() {
+        return new Object[] {
+                new Object[] {"강남", true},
+                new Object[] {null, false},
+                new Object[] {" ", false},
+        };
     }
 }
